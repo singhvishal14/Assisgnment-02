2677ee46ea0379a8af4eaf9158c73eb3f729c8aa
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-14 13:42:42 +0530

    Assisgnment02

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int ID;
+            string gender;
+            DateTime doj;
+            double salary;
+            Console.WriteLine("Enter ID");
+            ID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Empolyee Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Gender");
+            gender = Console.ReadLine();
+            Console.WriteLine("Enter Empolyee Salary");
+            salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("ID : \t" + ID + "\n Employee Name: \t" + name + "\n Empolyee Gender: \t"
+                + gender + "\n Empolee Salary: \t  " + salary + "\n DOJ: \t " + doj);
+            if (salary > 90000)
+            {
+                Console.WriteLine("you have to pay 30% tax");
+            }
+            else
+            {
+                Console.WriteLine("you have to pay 15% tax");
+                Console.ReadKey();
+            }
+        }
+    }
+}
